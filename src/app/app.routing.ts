dfd1af0b2ac7d4ea07363d3b3c0640d1c25c966c
Vusumzi123 <D.O.GeeVz@gmail.com>
2019-11-21 22:54:30 -0600

    Added main Files

---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home',             component: ComponentsComponent },
+    /* { path: 'user-profile',     component: ProfileComponent },
+    { path: 'signup',           component: SignupComponent },
+    { path: 'landing',          component: LandingComponent },
+    { path: 'nucleoicons',      component: NucleoiconsComponent } */
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
